c4e7dd7d68078e56eaf50eddcec7711d48918779
Oleksandr <netesa.sasha@gmail.com>
2024-07-27 11:45:53 +0300

    /4

--- src/js/projects/projects.js
+++ src/js/projects/projects.js
@@ -1,10 +1,18 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-const btnNextEl = document.querySelector('.swiper-button-next');
+const btnNextEl = document.querySelector('.swiper-button-next ');
 btnNextEl.style.color = '#FAFAFA';
-btnNextEl.style.background = '#1C1D20';
-btnNextEl.style.padding = '18px';
+btnNextEl.style.position = 'static';
+btnNextEl.style.width = '64px';
+btnNextEl.style.height = '64px';
+btnNextEl.style.margin = '0';
+const btnPrevEl = document.querySelector('.swiper-button-prev ');
+btnPrevEl.style.position = 'static';
+btnPrevEl.style.width = '64px';
+btnPrevEl.style.height = '64px';
+btnPrevEl.style.color = '#FAFAFA';
+btnPrevEl.style.margin = '0';
 export const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 30,
