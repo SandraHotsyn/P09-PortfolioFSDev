d4ca59fa011b7882f3b0e49d2f65f41182c97fec
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-07-30 14:34:08 +0100

    add border to accordeon

--- src/js/faq/faq.js
+++ src/js/faq/faq.js
@@ -11,6 +11,12 @@ export function initializeAccordion() {
       panelClass: 'ac-panel',
       activeClass: 'is-active',
     });
+
+    applyStyles('.ac-trigger', {
+      position: 'relative',
+    });
+
+    applyCustomStyles();
   });
 }
 
@@ -20,6 +26,44 @@ const applyStyles = (selector, styles) => {
   });
 };
 
-applyStyles('.ac-trigger, .ac-text', { color: '#fafafa' });
+applyStyles('.ac-trigger, .ac-text', { color: '#fafafacc' });
 applyStyles('.ac-panel, .ac', { backgroundColor: '#1c1d20' });
-applyStyles('.ac', { border: '0px solid #eee' });
+applyStyles('.ac', { border: '0px solid #fafafa' });
+
+const applyCustomStyles = () => {
+  const styleElement = document.createElement('style');
+  styleElement.textContent = `
+    .ac .ac-trigger::after {
+      content: '>';
+      text-align: center;
+      width: 34px;
+      height: 34px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 2px solid rgba(250, 250, 250, 0.2);
+      border-radius: 50%;
+      position: absolute;
+      right: 10px;
+      top: 50%;
+      transform: translateY(-50%);
+      pointer-events: none; /* Ensures the pseudo-element does not interfere with clicks */
+    }
+    
+    .ac.is-active > .ac-header .ac-trigger::after {
+      content: '^';
+    }
+
+    .ac .ac-trigger:focus {
+      color: #8a8a8a;
+    }
+
+    @media only screen and (min-width: 768px) {
+      .ac .ac-trigger::after {
+        width: 40px;
+        height: 40px;
+      }
+    }
+  `;
+  document.head.append(styleElement);
+};
