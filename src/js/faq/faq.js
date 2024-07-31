c89862fac7845ab29c13c5c2ce10dd7eae58f413
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-07-31 07:28:27 +0100

    fixed accordion

--- src/js/faq/faq.js
+++ src/js/faq/faq.js
@@ -3,67 +3,21 @@ import 'accordion-js/dist/accordion.min.css';
 
 export function initializeAccordion() {
   document.addEventListener('DOMContentLoaded', () => {
-    new Accordion('.accordion-container', {
-      duration: 400,
-      showMultiple: true,
-      elementClass: 'ac',
-      triggerClass: 'ac-trigger',
-      panelClass: 'ac-panel',
-      activeClass: 'is-active',
+    document.querySelectorAll('.faq-list .faq-item').forEach(element => {
+      const header = element.querySelector('.faq-header');
+      const button = element.querySelector('.faq-btn-trigger');
+      const content = element.querySelector('.faq-content');
+      const openIcon = button.querySelector('.faq-btn-open');
+      const closeIcon = button.querySelector('.faq-btn-close');
+      openIcon.style.display = 'block';
+      closeIcon.style.display = 'none';
+      content.style.display = 'none';
+      header.addEventListener('click', () => {
+        const isVisible = content.style.display === 'block';
+        content.style.display = isVisible ? 'none' : 'block';
+        openIcon.style.display = isVisible ? 'block' : 'none';
+        closeIcon.style.display = isVisible ? 'none' : 'block';
+      });
     });
-
-    applyStyles('.ac-trigger', {
-      position: 'relative',
-    });
-
-    applyCustomStyles();
   });
 }
-
-const applyStyles = (selector, styles) => {
-  document.querySelectorAll(selector).forEach(element => {
-    Object.assign(element.style, styles);
-  });
-};
-
-applyStyles('.ac-trigger, .ac-text', { color: '#fafafacc' });
-applyStyles('.ac-panel, .ac', { backgroundColor: '#1c1d20' });
-applyStyles('.ac', { border: '0px solid #fafafa' });
-
-const applyCustomStyles = () => {
-  const styleElement = document.createElement('style');
-  styleElement.textContent = `
-    .ac .ac-trigger::after {
-      content: '>';
-      text-align: center;
-      width: 34px;
-      height: 34px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border: 2px solid rgba(250, 250, 250, 0.2);
-      border-radius: 50%;
-      position: absolute;
-      right: 10px;
-      top: 50%;
-      transform: translateY(-50%);
-      pointer-events: none; /* Ensures the pseudo-element does not interfere with clicks */
-    }
-    
-    .ac.is-active > .ac-header .ac-trigger::after {
-      content: '^';
-    }
-
-    .ac .ac-trigger:focus {
-      color: #8a8a8a;
-    }
-
-    @media only screen and (min-width: 768px) {
-      .ac .ac-trigger::after {
-        width: 40px;
-        height: 40px;
-      }
-    }
-  `;
-  document.head.append(styleElement);
-};
